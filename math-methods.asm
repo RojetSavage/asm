2929100e1001a81a5f8e00fd647977b6d1456d9f
Robert Hurley <robert-hurley@hotmail.com.au>
2023-02-02 19:29:33 +1100

    [math] adds Fibnoacci method

--- math-methods.asm
+++ math-methods.asm
@@ -1,7 +1,46 @@
 /* Math Methods */
 .global Math.isPerfectSquare
 .global Math.Modulo
+.global Math.Exponent
+.global Math.Fibonacci
 
+
+Math.Fibonacci:
+	@param r0 - number in sequence to calculate
+	@param r1 - return value
+	
+	@r3 - store n-2th value
+	@r4 - store n-1th value
+	@r5 - store n
+
+	mov r3, #1
+	mov r4, #1
+	
+	cmp r0, #1	//if n = 1 or 2- goto basecase
+	beq fibBaseCase
+	cmp r0, #2	//if n = 1 or 2- goto basecase
+	beq fibBaseCase
+
+	sub r0, #2		//set 2nd iteration 	
+	sub r0,r0,#1	//minus 1 each iteration e.g 3 by the first time.	
+	add r5, r4, r3		
+
+	fibLoop:
+		ldr r3, [r4]
+		ldr r4, [r5]
+		add r5, r3, r4		
+		
+		sub r0, #1	//decrement count
+		cmp r0, #0
+		beq fibExit
+	b fibloop	
+
+	fibBaseCase:
+	mov r1, #1	
+	
+	fibExit:
+
+br lx 
 
 
 /* Returns 1 if value is perfect square, 0 if not */
