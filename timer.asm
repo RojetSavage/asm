0e88690ca65f71316f4bae2830755e2aec81ecd7
Robert Hurley <robert-hurley@hotmail.com.au>
2023-02-03 19:15:53 +1100

    adds timer variation

--- timer.asm
+++ timer.asm
@@ -2,10 +2,21 @@
 @r2=number of loops
 
 .global TIMER
+.global TIMER_autoSetup
 
 TIMER:
   wait1$:
     sub r2,#1
     cmp r2,#0
     bne wait1$
+bx lr
+
+
+TIMER_autoSetup:
+    mov r2,#0xFF00000
+    orr r2, #0x00FF00
+    orr r2, #0x00000FF
+    push {lr}
+    bl TIMER
+    pop {lr}
 bx lr
