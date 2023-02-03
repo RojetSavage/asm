8419a0108bf0811752bd06ca1d0b444b235de827
Robert Hurley <robert-hurley@hotmail.com.au>
2023-02-03 19:16:18 +1100

    refactor main to run tests

--- main.asm
+++ main.asm
@@ -3,75 +3,30 @@
 .global	_start
 
 _start:
-    @mov sp,#0xFFFFFFFF     //Make Space on The Stack
-    //ldr r1, =myArray
-    //ldr r8, [r1, #20]
+    @mov sp,#0xFFFFFFFF     //Make Space on The Stack  
+
+    bl TEST_ARRAY_CONTAINS      
+    bl TIMER_autoSetup
+
+    bl TEST_ARRAY_SUM          
+    bl TIMER_autoSetup
+
+    bl TEST_ARRAY_MAX 
+    bl TIMER_autoSetup      
+
+    bl TEST_ARRAY_MIN         
+    bl TIMER_autoSetup
+
+    bl TEST_MATH_MODULO        
+    bl TIMER_autoSetup
+
+    bl TEST_MATH_ISPERFECTSQUARE
+    bl TIMER_autoSetup      
 
     
-
-    //Test Array.Contains
-    //ldr r0, =myArray
-    //mov r1, #6
-    //mov r2, #3
-    //bl Array.Contains
-    //push {r3}
-    //bl _SETUP_LED
-    //pop {r1}
-    //bleq _FLASH
-
-    //Test Array.Sum
-    //ldr r0, =myArray
-    //mov r1, #3
-    //bl Array.Sum
-    //push {r2}
-    //bl _SETUP_LED
-    //pop {r1}
-    //bleq _FLASH
-
-    //Test Array.Min
-    //ldr r0, =myArray
-    //mov r1, #3
-    //bl Array.Min
-    //push {r2}
-    //bl _SETUP_LED
-    //pop {r1}
-    //bleq _FLASH
-
-    //Test Array.Max
-    //ldr r0, =myArray
-    //mov r1, #3
-    //bl Array.Max
-    //push {r2}
-    //bl _SETUP_LED
-    //pop {r1}
-    //bleq _FLASH
-
-    //Test Math.isPerfectSquare
-    //mov r0, #16
-    //bl Math.isPerfectSquare
-    //push {r1}
-    //bl _SETUP_LED
-    //pop {r1}
-    //bleq _FLASH   //bleq?? bl?
-
-    //Test Math.Modulo
-    //mov r0, #14
-    //mov r1, #6
-    //bl Math.Modulo
-    //push {r2}
-    //bl _SETUP_LED
-    //pop {r1}
-    //bl _FLASH
-
     wait:
     b wait
-    
-
 
 mov	r0, #0		@ return code 
 mov	r7, #248	@ exit 
 svc	#0
-
-
-.data
-myArray: .int 8,7,9
